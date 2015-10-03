0f4e691baad548c19380186e3209072b053ee913
Paritosh Gupta <paritosh@idelivr.in>
2015-10-04 01:39:36 +0530

    edit settings

--- v1/customer/models.py
+++ v1/customer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	first_name = models.CharField(verbose_name='First Name', max_length=30, blank=True, null=True)
+	last_name = models.CharField(verbose_name='Last Name', max_length=30, blank=True, null=True)
+	
